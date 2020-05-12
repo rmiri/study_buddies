9151746a900070ee78926a04ea11dd641821d685
github username <github email address>
2020-05-12 23:21:04 +0100

    fixed gemfile bcrypt

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,7 +8,7 @@ class User < ApplicationRecord
     validates :email, :uniqueness => true, on: :create
     validates :email, :presence => true
     validates_format_of :email, :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i
-    # validates :password, presence: true 
+    validates :password, presence: true 
 
     def created_study_groups
         self.study_groups.select { |sg| sg.user == self } 
