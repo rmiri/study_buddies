0c9338a3ec343abea5ec61bda1fbb2f88d13d3f6
github username <github email address>
2020-05-12 22:52:38 +0100

    updated seed

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,14 +1,14 @@
 class User < ApplicationRecord
     has_many :participants, dependent: :destroy 
     has_many :study_groups, dependent: :destroy 
-    has_many :study_groups, through: :participants, dependent: :destroy 
+    has_many :study_groups, through: :participants
     has_secure_password 
 
     validates :name, presence: true
     validates :email, :uniqueness => true, on: :create
     validates :email, :presence => true
     validates_format_of :email, :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i
-    validates :password, presence: true 
+    # validates :password, presence: true 
 
     def created_study_groups
         self.study_groups.select { |sg| sg.user == self } 
